4a512fcd599fad8381fe163796bb61265d807c8b
Eduardo Martínez <emartinezr161@outlook.com>
2023-11-15 21:43:21 +0100

    #38; definir relaciones de courses, crear course_user

--- database/migrations/2023_11_15_200702_create_courses_table.php
+++ database/migrations/2023_11_15_200702_create_courses_table.php
@@ -17,7 +17,11 @@ return new class extends Migration
             $table->string('title');
             $table->string('subtitle');
             $table->text('description');
-            $table->enum('status', [Course::BORRADOR, Course::REVISION, Course::PUBLICADO])->default(Course::BORRADOR);
+            $table->enum('status', [
+                Course::BORRADOR,
+                Course::REVISION,
+                Course::PUBLICADO
+                ])->default(Course::BORRADOR);
             $table->string('slug');
 
             $table->foreignId('user_id')
