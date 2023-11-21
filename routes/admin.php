d3f203a6c432b7d620bdd3a664f454e673f42cf5
Eduardo Martínez <emartinezr161@outlook.com>
2023-11-21 18:35:59 +0100

    admin layout y vista admin.dashboard

--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', function () {
-    return "Hola que tal";
+Route::get('/', function () {
+    return view('admin.dashboard');
 });
