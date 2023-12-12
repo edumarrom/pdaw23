b72a24985223c17987e2933fece0cc4108e2245f
Eduardo Martínez <emartinezr161@outlook.com>
2023-12-12 22:29:09 +0100

    cambios al estilo

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,14 +1,14 @@
-<x-app-layout>
-    <section class="bg-gray-700 py-12" style="background-image: linear-gradient(to right, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4)), url({{ Storage::url($course->image->path) }})">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2 gap-6">
+º<x-app-layout>
+    <section class="bg-cover {{-- bg-right-top bg-no-repeat --}} py-12" style="background-image: linear-gradient(to right, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4)), url({{Storage::url($course->image->path) }})">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
             <figure>
-                <img src="{{Storage::url($course->image->path)}}" alt="">
+                <img class="h-80 w-full object-cover rounded border-4 border-gray-300 border-s-white" src="{{Storage::url($course->image->path)}}" alt="">
             </figure>
 
-            <div>
-                <h1 class="text-4xl text-white font-bold">{{$course->title}}</h1>
-                <h2 class="text-lg text-white mt-2">{{$course->subtitle}}</h2>
-                <ul class="text-lg text-white mt-8">
+            <div class="text-gray-100">
+                <h1 class="text-4xl font-bold">{{$course->title}}</h1>
+                <h2 class="text-xl mt-2">{{$course->subtitle}}</h2>
+                <ul class="text-lg mt-8">
                     <li title="Categoría">
                         <i class="fa fa-solid fa-layer-group text-xl mr-2"></i>
                         {{$course->category->name}}
@@ -24,7 +24,7 @@
                     <li>
                         @for ($i = 1; $i <= 5; $i++)
                             @if ($course->rating >= $i)
-                                <i class="fa-solid fa-star text-white"></i>
+                                <i class="fa-solid fa-star"></i>
                             @else
                                 <i class="fa-solid fa-star text-gray-500"></i>
                             @endif
