301db3a174cef993bd5afc8c0c1ad63942bcdbf5
Eduardo Martínez <emartinezr161@outlook.com>
2023-12-17 16:45:16 +0100

    apartado requisitos/objetivos, apartado de secciones y lecciones

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,4 +1,4 @@
-º<x-app-layout>
+<x-app-layout>
     <section class="bg-cover {{-- bg-right-top bg-no-repeat --}} py-12" style="background-image: linear-gradient(to right, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.4)), url({{Storage::url($course->image->path) }})">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
             <figure>
@@ -35,4 +35,76 @@
             </div>
         </div>
     </section>
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-3">
+
+        <div class="col-span-2">
+            <section>
+                <h3 class="text-3xl font-bold mt-6 mb-2">
+                    <i class="fa fa-solid fa-flag-checkered rotate-45 mr-2"></i>
+                    Objetivos de prendizaje
+                </h3>
+                <div class="text-gray-700 bg-white shadow rounded p-4 space-y-4">
+                    <ul class="grid grid-cols-2 gap-x-6 gap-y-2">
+                        @foreach ($course->goals as $goal)
+                            <li class="">
+                                <i class="fa fa-solid fa-caret-right text-lg mr-2"></i>
+                                {{$goal->name}}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </section>
+
+            <section>
+                <h3 class="text-3xl font-bold mt-6 mb-2">
+                    <i class="fa fa-solid fa-vial mr-2"></i>
+                    Requisitos previos
+                </h3>
+                <div class="text-gray-700 bg-white shadow rounded p-4 space-y-4">
+                    <ul class="grid grid-cols-2 gap-x-6 gap-y-2">
+                        @foreach ($course->requirements as $requirement)
+                            <li class="">
+                                <i class="fa fa-solid fa-caret-right text-lg mr-2"></i>
+                                {{$requirement->name}}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </section>
+
+            <section>
+            <h3 class="text-3xl font-bold mt-6 mb-2">
+                <i class="fa fa-solid fa-book rotate-45 mr-2"></i>
+                Contenido del curso
+            </h3>
+
+            <div class="grid grid-cols-1 text-gray-700 bg-white rounded divide-y-2">
+                @foreach ($course->sections as $section)
+                    <article class="py-2">
+                        <header class="px-4 py-2 cursor-pointer">
+                            <h4 class="text-lg font-bold underline">{{ $section->title }}</h4>
+                        </header>
+
+                        <div>
+                            <ul>
+                                @foreach ($section->lessons as $lesson)
+                                    <a href="{{-- ruta al visor de lección --}}">
+                                        <li class="flex px-4 py-2 hover:bg-gray-200">
+                                            <i class="fa fa-solid fa-play-circle text-lg mr-2"></i>
+                                            {{ $lesson->title }}
+                                        </li>
+                                    </a>
+                                @endforeach
+                            </ul>
+                        </div>
+
+                    </article>
+                @endforeach
+            </div>
+
+            </section>
+        </div>
+
+    </div>
 </x-app-layout>
