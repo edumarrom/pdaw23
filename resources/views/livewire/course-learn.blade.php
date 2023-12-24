f8da86a306e0e49ae47b892e1a8a1202cefae0c6
Eduardo Martínez <emartinezr161@outlook.com>
2023-12-24 13:30:42 +0100

    clases css personalizadas para el iframe, incluir descripcion y boton de completado

--- resources/views/livewire/course-learn.blade.php
+++ resources/views/livewire/course-learn.blade.php
@@ -4,11 +4,27 @@
         {{-- Columna izquierda--}}
         <div class="col-span-2">
 
-            {!! $lesson->iframe !!}
+            <div class="embed-responsive rounded">
+                {!! $lesson->iframe !!}
+            </div>
+
             <h1 class="text-2xl text-gray-700 font-bold">
                 {{ $lesson->title }}
             </h1>
-            <div>
+
+            @if ($lesson->description)
+                <div class="text-gray-700">
+                    {{ $lesson->description->description }}
+                </div>
+            @endif
+
+            <label class="relative inline-flex items-center mt-2 mb-5 cursor-pointer">
+                <input type="checkbox" value="" class="sr-only peer">
+                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
+                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Marcar lección como completa</span>
+            </label>
+
+            <div class="flex justify-between text-gray-700 bg-white shadow-lg rounded-lg p-4">
                 @if ($previous)
                 <x-link-button href="{{route('courses.learn', [$course, $previous])}}">
                     Anterior
