efb6538235f65c79d83bd48bd4bcf4e64cb250f8
Eduardo Martínez <emartinezr161@outlook.com>
2023-12-24 18:47:20 +0100

    visualizar leccion  actual y completas en la lista de lecciones; hacer responsiva la página

--- resources/views/livewire/course-learn.blade.php
+++ resources/views/livewire/course-learn.blade.php
@@ -1,15 +1,15 @@
 <div class="mt-8">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-3 gap-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-3 lg:gap-8">
 
         {{-- Columna izquierda--}}
-        <div class="col-span-2">
+        <div class="col-span-2 mb-6">
 
-            <div class="embed-responsive rounded">
+            <div class="embed-responsive rounded mb-4">
                 {!! $lesson->iframe !!}
             </div>
 
             <h1 class="text-2xl text-gray-700 font-bold">
-                {{ $lesson->title }}
+                <span>{{$index+1}}. </span>{{ $lesson->title }}
             </h1>
 
             @if ($lesson->description)
@@ -21,7 +21,7 @@
             <label class="relative inline-flex items-center mt-2 mb-5 cursor-pointer">
                 <input type="checkbox" value="" class="sr-only peer">
                 <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
-                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Marcar lección como completa</span>
+                <span class="ms-3 text-base font-medium text-gray-700 dark:text-gray-300">Marcar lección como completa</span>
             </label>
 
             <div class="flex justify-between text-gray-700 bg-white shadow-lg rounded-lg p-4">
@@ -69,18 +69,38 @@
 
             <ul class="mt-4">
                 @foreach ($course->sections as $section)
-                    <li>
-                        <span class="font-bold text-rose-600">[{{ $section->id }}]</span>
-                        <a class="font-bold" href="">
-                            {{ Str::limit($section->title, 30) }}
+                    <li class="text-gray-700 mb-4">
+                        {{-- <span class="font-bold text-rose-600">[{{ $section->id }}]</span> --}}
+                        <a class="inline-block mb-2 font-bold" href="">
+                            {{ Str::limit($section->title, 40) }}
                         </a>
                         <ul>
                             @foreach ($section->lessons as $lesson)
-                                <li>
-                                    <i class="fa-solid fa-play-circle mr-2"></i>
-                                    <a href="{{ route('courses.learn', [$course, $lesson]) }}">
-                                        <span class="font-bold text-rose-400">[{{ $lesson->id }}]</span>
-                                        {{ Str::limit($lesson->title, 30) }}
+                                <li class="flex">
+                                    <div>
+                                        <a href="{{ route('courses.learn', [$course, $lesson]) }}">
+
+                                        @if ($lesson->completed)
+                                            @if ($lesson->id == $this->lesson->id)
+                                                <span class="inline-block w-5 h-5 rounded-full border-4 border-indigo-400 mt-1 mr-2"></span>
+                                            @else
+                                                <span class="inline-block w-5 h-5 rounded-full bg-indigo-400 mt-1 mr-2"></span>
+                                            @endif
+                                        @else
+
+                                            @if ($lesson->id == $this->lesson->id)
+                                                <span class="inline-block w-5 h-5 rounded-full border-4 border-gray-500 mt-1 mr-2"></span>
+                                            @else
+                                                <span class="inline-block w-5 h-5 rounded-full bg-gray-500 mt-1 mr-2"></span>
+                                            @endif
+
+                                        @endif
+
+                                    </div>
+                                    {{-- <i class="fa-solid fa-play-circle mr-2"></i> --}}
+
+                                        {{-- <span class="font-bold text-rose-400">[{{ $lesson->id }}]</span> --}}
+                                        {{ Str::limit($lesson->title, 40) }}
                                     </a>
                                 </li>
 
