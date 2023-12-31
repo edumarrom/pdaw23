8c307cb0253cf9e521c58dc36058d16375219403
Eduardo Martínez <emartinezr161@outlook.com>
2023-12-31 20:23:55 +0100

    mostrar mensaje si la busqueda no tiene resultados

--- resources/views/livewire/admin-users.blade.php
+++ resources/views/livewire/admin-users.blade.php
@@ -10,66 +10,76 @@
         </div>
     </div>
 
-    <div class="relative overflow-x-auto rounded-lg shadow-xl">
-        <table class="w-full text-sm text-left text-gray-500">
-            <thead class="text-xs text-gray-700 uppercase bg-blue-100">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        ID
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Foto
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Nombre
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Email
-                    </th>
-                    <th scope="col" class="wpx-6 py-3 md:w-1/3 lg:w-1/6">
-                        Acciones
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
-                    <tr class="bg-white border-b hover:bg-gray-50">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                            {{ $user->id }}
+    @if ($users->count())
+        <div class="relative overflow-x-auto rounded-lg shadow-xl">
+            <table class="w-full text-sm text-left text-gray-500">
+                <thead class="text-xs text-gray-700 uppercase bg-blue-100">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            ID
                         </th>
-                        <td class="px-6 py-4">
-                            <img class="w-auto h-12 rounded-full" src="{{ $user->profile_photo_url }}" alt="">
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $user->name }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $user->email }}
-                        </td>
-                        <td class="px-6 py-4">
-                            <a href="{{ route('admin.users.edit', $user) }}">
-                                <i class="fa-solid fa-pen"></i>
-                                Editar
-                            </a>
-                            {{-- <form action="{{ route('admin.users.destroy', $user) }}" method="post"
-                                id='delete-form-{{ $user->id }}'>
-                                @csrf
-                                @method('delete')
-                                <button type="button" class="text-red-500"
-                                    onclick="deleteUser({{ $user->id }})">
-                                    <i class="fa-solid fa-trash"></i>
-                                    Borrar
-                                </button>
-                            </form> --}}
-                        </td>
+                        <th scope="col" class="px-6 py-3">
+                            Foto
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Nombre
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Email
+                        </th>
+                        <th scope="col" class="wpx-6 py-3 md:w-1/3 lg:w-1/6">
+                            Acciones
+                        </th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr class="bg-white border-b hover:bg-gray-50">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                {{ $user->id }}
+                            </th>
+                            <td class="px-6 py-4">
+                                <img class="w-auto h-12 rounded-full" src="{{ $user->profile_photo_url }}" alt="">
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $user->name }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $user->email }}
+                            </td>
+                            <td class="px-6 py-4">
+                                <a href="{{ route('admin.users.edit', $user) }}">
+                                    <i class="fa-solid fa-pen"></i>
+                                    Editar
+                                </a>
+                                {{-- <form action="{{ route('admin.users.destroy', $user) }}" method="post"
+                                    id='delete-form-{{ $user->id }}'>
+                                    @csrf
+                                    @method('delete')
+                                    <button type="button" class="text-red-500"
+                                        onclick="deleteUser({{ $user->id }})">
+                                        <i class="fa-solid fa-trash"></i>
+                                        Borrar
+                                    </button>
+                                </form> --}}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 
-    <div class="mt-6">
-        {{ $users->links() }}
+        <div class="mt-6">
+            {{ $users->links() }}
+        </div>
+    @else
+    <div class="flex items-center justify-center p-4">
+        <div class="flex flex-col items-center">
+            <img class="w-64 h-auto mb-4" src="{{ asset('img/tumbleweed.png') }}" alt="">
+            {{-- Source: https://www.creativefabrica.com/product/cactus-tumbleweed-2/ref/154380/ --}}
+            <h3 class="text-lg font-bold text-gray-600">No hay usuarios que coincidan con tu búsqueda</h3>
+        </div>
     </div>
+    @endif
 
 </div>
