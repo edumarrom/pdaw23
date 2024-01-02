0c4f71c20df8d6059ddf0aef46af3025e3d3c4c0
Eduardo Martínez <emartinezr161@outlook.com>
2024-01-02 16:54:02 +0100

    restringir ruta cursos del profesor

--- routes/teacher.php
+++ routes/teacher.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\TeacherCourses;
 
 Route::get('courses', TeacherCourses::class)
+    ->middleware('can:course-read')
     ->name('courses.index');
