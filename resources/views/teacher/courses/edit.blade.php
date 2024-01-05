d0bc4b3b36abc40ebb8790c9edd8f02b53cc5aa0
Eduardo Martínez <emartinezr161@outlook.com>
2024-01-05 21:18:17 +0100

    comienzo formulario de edición

--- resources/views/teacher/courses/edit.blade.php
+++ resources/views/teacher/courses/edit.blade.php
@@ -1,3 +1,42 @@
 <x-app-layout>
-    Editar el curso {{ $course->title }}
+    <div class="container py-8">
+
+        <div class="card">
+
+            <div class="px6 py4 text-gray-700">
+                <h2 class="text-2xl font-bold">Información del curso</h2>
+                <hr class="mbt-2 mb-6">
+
+                <form action="{{ route('teacher.courses.update', $course) }}" method="post">
+                    @csrf
+                    @method('PUT')
+
+                    <div class="mb-4">
+                        <x-label for="title" class="mb-2"  value="Título del curso" />
+                        <x-input id="title"
+                                 name="title"
+                                 type="text"
+                                 required
+                                 class="block w-full mb-2"
+                                 placeholder="Escribe un título para este curso"
+                                 value="{{ old('title', $course->title) }}" />
+                    </div>
+
+                    <div class="mb-4">
+                        <x-label for="slug" class="mb-2"  value="Slug del curso" />
+                        <x-input id="slug"
+                                 name="slug"
+                                 type="text"
+                                 required
+                                 class="block w-full mb-2"
+                                 placeholder="Escribe un slug para este curso"
+                                 value="{{ old('slug', $course->slug) }}" />
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+
+    </div>
 </x-app-layout>
