89fcf3594825716d7c816cdcc743cb70612b8c33
Eduardo Martínez <emartinezr161@outlook.com>
2024-01-11 07:49:33 +0100

    comienzo area para editar lección

--- app/Livewire/Teacher/CoursesLesson.php
+++ app/Livewire/Teacher/CoursesLesson.php
@@ -2,20 +2,39 @@
 
 namespace App\Livewire\Teacher;
 
+use App\Models\Lesson;
 use App\Models\Section;
 use Livewire\Component;
 
 class CoursesLesson extends Component
 {
     public $section;
+    public $lesson;
+
+    protected $rules = [
+        'lesson.title' => 'required',
+        'lesson.platform_id' => 'required',
+        'lesson.path' => 'required',
+    ];
 
     public function mount(Section $section)
     {
         $this->section = $section;
+        $this->lesson = new Lesson();
     }
 
     public function render()
     {
         return view('livewire.teacher.courses-lesson');
+    }
+
+    public function editLesson($id)
+    /*
+    ¿Por qué da un ArrayToString Exception?
+    {
+        $this->lesson = $lesson;
+    } */
+    {
+        $this->lesson = Lesson::find($id);
     }
 }
--- resources/views/livewire/teacher/courses-lesson.blade.php
+++ resources/views/livewire/teacher/courses-lesson.blade.php
@@ -1,50 +1,66 @@
 <div>
+
     @foreach ($section->lessons as $item)
 
         <article class="card border border-gray-100 shadow-md mt-4">
 
             <div class="px-4 py-2">
 
-                <header>
-                    <h4>
-                        <i class="fa fa-solid fa-play-circle mr-1"></i>
-                        {{ $item->title }}
-                    </h4>
-                </header>
+                @if ($lesson->id == $item->id)
+                    editar la lección
+                @else
+                    <header>
+                        <h4>
+                            <i class="fa fa-solid fa-play-circle mr-1"></i>
+                            {{ $item->title }}
+                        </h4>
+                    </header>
 
-                <div>
+                    <div>
 
-                    <hr class="my-2">
+                        <hr class="my-2">
 
-                    <p class="text-sm">
-                        Plataforma:
-                        @switch( Str::lower($item->platform->name) )
+                        <p class="text-sm">
+                            Plataforma:
+                            @switch( Str::lower($item->platform->name) )
+                                @case('youtube')
+                                    <span class="text-red-500">
+                                        <i class="fa-brands fa-youtube"></i>
+                                        <span class="font-medium">{{ $item->platform->name }}</span>
+                                    </span>
+                                    @break
+                                @case('vimeo')
+                                    <span class="text-blue-400">
+                                        <i class="fa-brands fa-vimeo"></i>
+                                        <span class="font-medium">{{ $item->platform->name }}</span>
+                                    @break
+                                @default
+                                        <span class="font-medium">{{ $item->platform->name }}</span>
+                                    </span>
+                            @endswitch
+                        </p>
+                        <p class="text-sm">
+                            Enlace:
+                            <a href="{{ $item->path }}" target="_blank"
+                                class="text-indigo-500">
+                                {{ $item->path }}
+                            </a>
+                        </p>
+                    </div>
 
-                            @case('youtube')
-                                <span class="text-red-500">
-                                    <i class="fa-brands fa-youtube"></i>
-                                    <span class="font-medium">{{ $item->platform->name }}</span>
-                                </span>
-                                @break
-                            @case('vimeo')
-                                <span class="text-blue-400">
-                                    <i class="fa-brands fa-vimeo"></i>
-                                    <span class="font-medium">{{ $item->platform->name }}</span>
-                                @break
-                            @default
-                                    <span class="font-medium">{{ $item->platform->name }}</span>
-                                </span>
+                    <div class="flex justify-end">
+                        <x-secondary-button type="button" class="hover:text-white hover:bg-indigo-500 mr-2"
+                                wire:click="editLesson({{ $item->id }})">
+                            <i class="fa-solid fa-pen mr-1"></i>
+                            Editar
+                        </x-secondary-button>
 
-                        @endswitch
-                    </p>
-                    <p class="text-sm">
-                        Enlace:
-                        <a href="{{ $item->path }}" target="_blank"
-                            class="text-indigo-500">
-                            {{ $item->path }}
-                        </a>
-                    </p>
-                </div>
+                        <x-secondary-button type="button" class="hover:text-white hover:bg-rose-600">
+                            <i class="fa-solid fa-trash mr-1"></i>
+                            Eliminar
+                        </x-secondary-button>
+                    </div>
+                @endif
 
             </div>
 
