7d4c81b599d01dc517735ffc4bdc04e703547a48
Eduardo Martínez <emartinezr161@outlook.com>
2024-01-15 16:54:22 +0100

    habilitar buscador

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Course;
 use Livewire\Component;
 
 class Search extends Component
@@ -11,5 +12,13 @@ class Search extends Component
     public function render()
     {
         return view('livewire.search');
+    }
+
+    public function getResultsProperty()
+    {
+        return Course::where('title', 'ILIKE', '%' . $this->search . '%')
+            ->where('status', 3)
+            ->take(5)
+            ->get();
     }
 }
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -6,12 +6,25 @@
                     type="search"
                     name="search"
                     placeholder="Vue para principantes..."
-                    wire:model.live="search">
-            <x-button class="absolute rounded-l-none right-0 top-0 h-10 mt-2">Buscar</x-button>
+                    wire:model.live.debounce.500="search">
+            <x-button type="button" class="absolute rounded-l-none right-0 top-0 h-10 mt-2 cursor-auto">Buscar</x-button>
 
-            <ul class="absolute w-full left-0 mt-1 text-sm overflow-hidden bg-white rounded-md shadow-md">
-                <li class="leading-8 pl-2 hover:bg-gray-100"> {{ $search }}</li>
-            </ul>
+            @if ($search)
+                <ul class="absolute w-full left-0 mt-1 text-sm overflow-hidden bg-white rounded-md shadow-md">
+                    @forelse ($this->results as $result)
+                        <li class="leading-8 pl-2 hover:bg-gray-100">
+                            <a href="{{ route('courses.show', $result) }}"
+                                    class="block cursor-pointer">
+                                {{ $result->title }}
+                            </a>
+                        </li>
+                    @empty
+                        <li class="leading-8 pl-2 hover:bg-gray-100">
+                            No se encontraron resultados para "{{ $search }}"
+                        </li>
+                    @endforelse
+                </ul>
+            @endif
 
         </div>
     </form>
