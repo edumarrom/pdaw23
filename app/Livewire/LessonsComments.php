50d7f808bca7e24d8f23a3fc7d0afb4112cafbf6
Eduardo Martínez <emartinezr161@outlook.com>
2024-01-16 19:25:54 +0100

    formulario de edicion de comentario

--- app/Livewire/LessonsComments.php
+++ app/Livewire/LessonsComments.php
@@ -2,38 +2,57 @@
 
 namespace App\Livewire;
 
+use App\Models\Comment;
 use Livewire\Component;
 
 class LessonsComments extends Component
 {
     public $lesson;
-    public $comment = '';
+    public $comment;
+
+    public $create_body;
+    public $body;
 
     public $rules = [
-        'comment' => 'required|',
+        'comment.body' => 'required',
     ];
 
     public function mount($lesson)
     {
         $this->lesson = $lesson;
+        $this->comment = new Comment();
     }
 
     public function render()
     {
-        return view('livewire.lessons-comments');
+        $comments = $this->lesson->comments()->orderBy('created_at', 'desc')->get();
+        return view('livewire.lessons-comments', compact('comments'));
     }
 
     public function store()
     {
-        $this->validate();
+        $this->validate([
+            'create_body' => 'required',
+        ]);
 
         $this->lesson->comments()->create([
-            'body' => $this->comment,
+            'body' => $this->create_body,
             'user_id' => auth()->id(),
         ]);
 
-        $this->reset('comment');
+        $this->reset('body');
 
         $this->lesson = $this->lesson->fresh();
+    }
+
+    public function edit(Comment $comment)
+    {
+        $this->comment = $comment;
+        $this->body = $comment->body;
+    }
+
+    public function cancel()
+    {
+        $this->comment = new Comment();
     }
 }
--- resources/views/livewire/lessons-comments.blade.php
+++ resources/views/livewire/lessons-comments.blade.php
@@ -15,20 +15,16 @@
                     </figure>
 
                     <div class="w-full ml-4 mb-2">
-                        <x-textarea id="comment"
-                                    name="comment"
+                        <x-textarea id="body"
+                                    name="body"
                                     class="w-full"
                                     rows="2"
                                     placeholder="Añade un comentario..."
-                                    wire:model.live="comment"></x-textarea>
-                        <x-input-error for="comment" class="mt-2" />
+                                    wire:model.live="create_body"></x-textarea>
+                        <x-input-error for="create_body" class="mt-2" />
                     </div>
                 </div>
                 <div class="flex justify-end">
-                    <x-secondary-button x-on:click="open = false">
-                        Cancelar
-                    </x-secondary-button>
-
                     <x-button color="indigo" class="ml-2">
                         Comentar
                     </x-button>
@@ -38,27 +34,77 @@
 
         </div>
 
-        @foreach ($lesson->comments as $comment)
+        @foreach ($comments as $item)
             <hr class="my-4">
-            <div class="flex mb-8">
-                <figure class="flex-shrink-0">
-                    <img class="h-12 w-12 object-cover rounded-full shadow-lg" src="{{ $comment->user->profile_photo_url }}" alt="">
-                </figure>
-                <div class="flex w-full">
-                    <article class="w-full">
-                        <div class="ml-4">
-                            <p class="text-gray-700 font-bold">{{ $comment->user->name }}</p>
-                            <p class="mb-2 text-xs text-gray-600">{{ $comment->created_at ? $comment->created_at->diffForHumans() : '' }}</p>
-                            <p class="text-sm text-gray-600">{{ $comment->body }}</p>
+            @if ($comment->id == $item->id)
+                <div class="px-4 py-2">
+
+                    <form wire:submit.prevent='update'>
+
+                        <div class="flex">
+                            <figure class="flex-shrink-0">
+                                <img class="h-12 w-12 object-cover rounded-full shadow-lg" src="{{ auth()->user()->profile_photo_url }}" alt="">
+                            </figure>
+
+                            <div class="w-full ml-4 mb-2">
+                                <x-textarea id="body"
+                                            name="body"
+                                            class="w-full"
+                                            rows="2"
+                                            placeholder="Añade un comentario..."
+                                            wire:model.live="comment.body"></x-textarea>
+                                <x-input-error for="comment.body" class="mt-2" />
+                            </div>
                         </div>
-                    </article>
-                    @can('author', $comment)
-                        <div>
-                            <i class="fa-solid fa-ellipsis-vertical"></i>
+                        <div class="flex justify-end">
+                            <x-secondary-button wire:click="cancel">
+                                Cancelar
+                            </x-secondary-button>
+
+                            <x-button color="indigo" class="ml-2">
+                                Comentar
+                            </x-button>
                         </div>
-                    @endcan
+
+                    </form>
+
                 </div>
-            </div>
+            @else
+                <div class="flex mb-8">
+                    <figure class="flex-shrink-0">
+                        <img class="h-12 w-12 object-cover rounded-full shadow-lg" src="{{ $item->user->profile_photo_url }}" alt="">
+                    </figure>
+                    <div class="flex w-full">
+                        <article class="w-full">
+                            <div class="ml-4">
+                                <p class="text-gray-700 font-bold">{{ $item->user->name }}</p>
+                                <p class="mb-2 text-xs text-gray-600">{{ $item->created_at ? $item->created_at->diffForHumans() : '' }}</p>
+                                <p class="text-sm text-gray-600">{{ $item->body }}</p>
+                            </div>
+                        </article>
+                        @can('author', $item)
+                            <x-dropdown align="right">
+                                <x-slot name="trigger">
+                                    <button class="w-8">
+                                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                                    </button>
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    <x-dropdown-link class="cursor-pointer" wire:click="edit({{ $item }})">
+                                        <i class="fa-solid fa-pen mr-1"></i>
+                                        Editar
+                                    </x-dropdown-link>
+                                    <x-dropdown-link class="cursor-pointer" wire:click="">
+                                        <i class="fa-solid fa-trash mr-1"></i>
+                                        Eliminar
+                                    </x-dropdown-link>
+                                </x-slot>
+                            </x-dropdown>
+                        @endcan
+                    </div>
+                </div>
+            @endif
         @endforeach
 
     </div>
