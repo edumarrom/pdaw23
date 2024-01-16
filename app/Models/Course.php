fe208de4cb180b46dd04af97703bf12c9795b9b8
Eduardo Martínez <emartinezr161@outlook.com>
2024-01-16 23:01:10 +0100

    closes #172; incluir timestamps al matricularse en un curso

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -80,7 +80,7 @@ class Course extends Model
      */
     public function students()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class)->withTimestamps();
     }
 
     public function reviews()
