8ca2ed1056b7f1e91dae481c38f5c4f4a0bb8954
Eduardo Martínez <emartinezr161@outlook.com>
2024-01-27 22:35:35 +0100

    limpiar script, ordenarlo y adaptarlo para que funcione al editar

--- resources/js/users/validation.js
+++ resources/js/users/validation.js
@@ -1,8 +1,16 @@
+const form = document.querySelector('#user-form');
+
 const request = {
   name: document.getElementById('name'),
   email: document.getElementById('email'),
   password: document.getElementById('password'),
   passwordConfirmation: document.getElementById('password_confirmation'),
+};
+
+const requirements = {
+  name: ['required'],
+  email: ['required', 'email'],
+  password: ['required', 'min:8', 'letters', 'numbers', 'symbols', 'confirmed'],
 };
 
 const trans = {
@@ -11,60 +19,23 @@ const trans = {
   password: 'contraseña',
 };
 
-const form = document.querySelector('#user-form');
 form.addEventListener('submit', validateForm);
-
-let errors = {};
-
-const errorMessages = {
-  required: (e) => `El campo ${e} es obligatorio.`,
-  email: (e) => `El campo ${e} no es un correo válido.`,
-  min: (e, n) => `El campo ${e} debe contener al menos ${n} caracteres.`,
-  letters: (e) => `El campo ${e} debe contener al menos una letra.`,
-  numbers: (e) => `El campo ${e} debe contener al menos un número.`,
-  symbols: (e) => `El campo ${e} debe contener al menos un símbolo.`,
-  confirmed: 'La confirmación de contraseña no coincide.',
-};
-
-for (const key in request) {
-  request[key].addEventListener('keyup', () => {
-    removeErrorMessage(key);
-  });
-}
 
 function validateForm(event) {
   event.preventDefault();
   errors = {};
 
+  if (window.location.href.endsWith('edit')) {
+    if (request.password.value !== '') {
+      requirements.password = ['min:8', 'letters', 'numbers', 'symbols', 'confirmed'];
+    } else {
+      delete requirements.password;
+    }
+  }
+
   validate({
-    name: ['required'],
-    email: ['required', 'email'],
-    password: ['required', 'min:8', 'letters', 'numbers', 'symbols', 'confirmed'],
+    ...requirements
   });
-
-   /* if (!required(request.name)) {
-    errors.name = errorMessages.required(trans.name);
-  } */
-
-  /* if (!required(request.email)) {
-    errors.email = errorMessages.required(trans.email);
-  } else if (!isEmail(request.email)) {
-    errors.email = errorMessages.email(trans.email);
-  } */
-
-  /* if (!required(request.password)) {
-    errors.password = errorMessages.required(trans.password);
-  } else if (!min(request.password, 8)) {
-    errors.password = errorMessages.min(trans.password, 8);
-  } else if (!letters(request.password)) {
-    errors.password = errorMessages.letters(trans.password);
-  } else if (!numbers(request.password)) {
-    errors.password = errorMessages.numbers(trans.password);
-  } else if (!symbols(request.password)) {
-    errors.password = errorMessages.symbols(trans.password);
-  } else if (!confirmed(request.password)) {
-    errors.password = errorMessages.confirmed;
-  } */
 
   if (Object.keys(errors).length > 0) {
     showErrors(errors);
@@ -136,6 +107,14 @@ function validate(elements) {
   });
 }
 
+let errors = {};
+
+for (const key in request) {
+  request[key].addEventListener('keyup', () => {
+    removeErrorMessage(key);
+  });
+}
+
 function showErrors(errors) {
   for (const key in errors) {
     removeErrorMessage(key);
@@ -168,3 +147,13 @@ const letters = (e) => /[a-zA-Z]/.test(e.value);
 const numbers = (e) => /\d/.test(e.value);
 const symbols = (e) => /[!@#$%^&*(),.?":{}|<>]/.test(e.value);
 const confirmed = (e) => e.value === request.passwordConfirmation.value;
+
+const errorMessages = {
+  required: (e) => `El campo ${e} es obligatorio.`,
+  email: (e) => `El campo ${e} no es un correo válido.`,
+  min: (e, n) => `El campo ${e} debe contener al menos ${n} caracteres.`,
+  letters: (e) => `El campo ${e} debe contener al menos una letra.`,
+  numbers: (e) => `El campo ${e} debe contener al menos un número.`,
+  symbols: (e) => `El campo ${e} debe contener al menos un símbolo.`,
+  confirmed: 'La confirmación de contraseña no coincide.',
+};
