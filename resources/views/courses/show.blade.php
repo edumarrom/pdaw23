40221c240bc7bd484487da53924bb59055ec0e41
Eduardo Martínez <emartinezr161@outlook.com>
2024-01-28 23:36:51 +0100

    actualizar previsualización de un curso

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -22,8 +22,8 @@
                         {{$course->students_count}}
                     </li>
                     <li>
+                        <i class="fa-solid fa-star text-xl mr-2"></i>
                         @if ($course->reviews_count > 0)
-                            <i class="fa-solid fa-star text-xl mr-2"></i>
                             {{$course->rating}} {{ $course->rating == 1 ? 'estrella' : 'estrellas' }}
                             ({{ $course->reviews_count }} {{ $course->reviews_count == 1 ? 'valoración' : 'valoraciones' }})
                         @else
