4c37e79e45b69a0273d1f2236e742f5e617ad089
Eduardo Martínez <emartinezr161@outlook.com>
2024-01-31 17:47:58 +0100

    traducir chips de roles en tabla usuarios

--- resources/views/livewire/admin/users-index.blade.php
+++ resources/views/livewire/admin/users-index.blade.php
@@ -49,17 +49,17 @@
                                     @switch($role->name)
                                         @case('admin')
                                             <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
-                                                {{ $role->name }}
+                                                {{ __($role->name) }}
                                             </span>
                                             @break
                                         @case('teacher')
                                             <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-purple-100 text-purple-800">
-                                                {{ $role->name }}
+                                                {{ __(Str::lower(__(Str::Headline($role->name)))) }}
                                             </span>
                                             @break
                                         @default
                                             <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-                                                {{ $role->name }}
+                                                {{ __($role->name) }}
                                             </span>
                                     @endswitch
                                 @endforeach
