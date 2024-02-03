0ef10b73fe43054d2999099aeadc284e394c1ab0
Eduardo Martínez <emartinezr161@outlook.com>
2024-02-03 15:31:39 +0100

    closes #45; solo mostrar los cursos publicados; incluir un buscador

--- resources/views/livewire/my-courses.blade.php
+++ resources/views/livewire/my-courses.blade.php
@@ -1,35 +1,23 @@
 <div class="container">
 
-    <div class="flex items-center justify-between mt-24">
-        <h1 class="text-3xl font-bold text-gray-700">Mis cursos</h2>
+    <div class="flex flex-col justify-between mt-24 md:flex-row">
+        <h1 class="mb-4 text-3xl font-bold text-gray-700">Mis cursos</h1>
+        <div class="flex items-center justify-between flex-column" >
+            <label for="table-search" class="sr-only">Buscar</label>
+            <div class="relative w-full">
+                <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <i class="fa fa-solid fa-search text-gray-500"></i>
+                </div>
+                <input id="table-search-courses" type="search" wire:model.live.debounce.750ms="search" placeholder="Buscar un curso"
+                    class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-full focus:ring-indigo-500 focus:border-indigo-500">
+            </div>
+        </div>
     </div>
+
 
     <div class="mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
 
         @foreach ($courses as $course)
-            {{-- <article class="card text-gray-700 rounded-xl font-semibold overflow-hidden mb-8">
-                <img src="{{$course->imagePath}}" alt="{{$course->title}}" class="mb-2">
-                <div class="flex flex-col flex-1 mx-2">
-                    <p class="text-lg font-semibold ">
-                        {{$course->title}}
-                    </p>
-                    <p>Por {{ $course->teacher->name }}</p>
-
-                    <div class="cursor-default">
-                        <ul class="flex text-xs mb-2">
-                            <li class="text-gray-500 mr-2">
-                                <i class="fa-solid fa-layer-group"></i>
-                                {{$course->category->name}}
-                            </li>
-
-                            <li class="text-gray-500 mr-2">
-                                <i class="fa-solid fa-cubes"></i>
-                                {{$course->level->name}}
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </article> --}}
             <x-course-card-min :course="$course"></x-course-card-min>
         @endforeach
 </div>
