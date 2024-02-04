8ba241c0376a7f8d382b8c7de0a5c443c8dee8bd
Eduardo Martínez <emartinezr161@outlook.com>
2024-02-04 16:48:57 +0100

    editar y eliminar valoraciones de un curso

--- resources/views/livewire/courses-reviews.blade.php
+++ resources/views/livewire/courses-reviews.blade.php
@@ -50,11 +50,10 @@
         @endif
 
         @can('enrolled', $course)
-
             @if($reviews->contains('user_id', auth()->id()))
-                <div class="card mb-4">
+                <div class="mb-4">
 
-                    <div  class="px-4 py-1  ">
+                    <div class="px-4 py-1 text-lg text-gray-700">
                         ¡Gracias por compartir tu opinión con nosotros!
                     </div>
                 </div>
@@ -67,12 +66,11 @@
                             <div class="mb-2">
                                 <span class="text-xl font-semibold mr-2">Valora este curso:</span>
 
-                                {{-- Hacer un checkbox que su label sea un icono de estrella de fontawesome  --}}
                                 <div class="inline-block">
                                     <ul class="flex text-xl">
                                         @for ($i = 1; $i <= 5; $i++)
-                                            <li class="mr-1 cursor-pointer" wire:click="$set('rating', {{ $i }})">
-                                                @if ($rating >= $i)
+                                            <li class="mr-1 cursor-pointer" wire:click="$set('create_rating', {{ $i }})">
+                                                @if ($create_rating >= $i)
                                                     <i class="fa fa-solid fa-star text-amber-400"></i>
                                                 @else
                                                     <i class="fa fa-solid fa-star text-gray-400"></i>
@@ -89,8 +87,8 @@
                                             class="block w-full mb-2"
                                             rows="3"
                                             placeholder="Cuéntanos qué te ha parecido este curso"
-                                            wire:model.live="comment"></x-textarea>
-                                <x-input-error for="comment" class="mt-2" />
+                                            wire:model.live="create_comment"></x-textarea>
+                                <x-input-error for="create_comment" class="mt-2" />
                             </div>
 
                             <div class="flex justify-end">
@@ -109,27 +107,96 @@
         @endcan
 
         <div class="card p-0 divide-y-2">
-            @forelse ($reviews as $review)
-                <article class="flex p-4">
-                    <figure class="mr-4">
-                        <img class="h-12 w-12 object-cover rounded-full shadow-lg" src="{{$review->user->profile_photo_url}}" alt="">
-                    </figure>
-                    <div class="flex-1">
-                        <div>
-                            <h2 class="text-xl font-bold">{{$review->user->name}}</h2>
-                            <ul class="flex text-sm">
-                                @for ($i = 1; $i <= 5; $i++)
-                                    @if ($review->rating >= $i)
-                                        <li class="mr-1"><i class="fa fa-solid fa-star text-amber-400"></i></li>
-                                    @else
-                                        <li class="mr-1"><i class="fa fa-solid fa-star text-gray-400"></i></li>
-                                    @endif
-                                @endfor
-                            </ul>
-                            {{$review->comment}}
-                        </div>
+            @forelse ($reviews as $item)
+
+                @if ($review->id == $item->id)
+                    <div  class="px-4 py-2  ">
+
+                        <form wire:submit.prevent='update'>
+                            <div class="mb-2">
+                                <span class="text-xl font-semibold mr-2">Valora este curso:</span>
+
+                                <div class="inline-block">
+                                    <ul class="flex text-xl">
+                                        @for ($i = 1; $i <= 5; $i++)
+                                            <li class="mr-1 cursor-pointer" wire:click="$set('review.rating', {{ $i }})">
+                                                @if ($review->rating >= $i)
+                                                    <i class="fa fa-solid fa-star text-amber-400"></i>
+                                                @else
+                                                    <i class="fa fa-solid fa-star text-gray-400"></i>
+                                                @endif
+                                            </li>
+                                        @endfor
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="mb-2">
+                                <x-textarea id="comment"
+                                            name="comment"
+                                            class="block w-full mb-2"
+                                            rows="3"
+                                            placeholder="Cuéntanos qué te ha parecido este curso"
+                                            wire:model.live="review.comment"></x-textarea>
+                                <x-input-error for="review.comment" class="mt-2" />
+                            </div>
+
+                            <div class="flex justify-end">
+                                <x-secondary-button wire:click="cancel">
+                                    Cancelar
+                                </x-secondary-button>
+
+                                <x-secondary-button type="submit" class="hover:bg-indigo-500 hover:text-white ml-2"
+                                        title="Enviar">
+                                    <i class="fa-solid fa-check mr-1"></i>
+                                    Enviar
+                                </x-secondary-button>
+                            </div>
+                        </form>
+
                     </div>
-                </article>
+                @else
+                    <article class="flex p-4">
+                        <figure class="mr-4">
+                            <img class="h-12 w-12 object-cover rounded-full shadow-lg" src="{{$item->user->profile_photo_url}}" alt="">
+                        </figure>
+                        <div class="flex-1">
+                            <div>
+                                <h2 class="text-xl font-bold">{{$item->user->name}}</h2>
+                                <ul class="flex text-sm">
+                                    @for ($i = 1; $i <= 5; $i++)
+                                        @if ($item->rating >= $i)
+                                            <li class="mr-1"><i class="fa fa-solid fa-star text-amber-400"></i></li>
+                                        @else
+                                            <li class="mr-1"><i class="fa fa-solid fa-star text-gray-400"></i></li>
+                                        @endif
+                                    @endfor
+                                </ul>
+                                {{$item->comment}}
+                            </div>
+                        </div>
+                        @can('author', $item)
+                            <x-dropdown align="right">
+                                <x-slot name="trigger">
+                                    <button class="w-8">
+                                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                                    </button>
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    <x-dropdown-link class="cursor-pointer" wire:click="edit({{ $item }})">
+                                        <i class="fa-solid fa-pen mr-1"></i>
+                                        Editar
+                                    </x-dropdown-link>
+                                    <x-dropdown-link class="cursor-pointer" wire:click="destroy({{ $item }})">
+                                        <i class="fa-solid fa-trash mr-1"></i>
+                                        Eliminar
+                                    </x-dropdown-link>
+                                </x-slot>
+                            </x-dropdown>
+                        @endcan
+                    </article>
+                @endif
             @empty
                 <div class="flex items-center justify-center p-4">
                     <div class="flex flex-col items-center">
@@ -138,10 +205,6 @@
                     </div>
                 </div>
             @endforelse
-        </div>
-
-        <div class="mt-6">
-            {{ $reviews->links(data: ['scrollTo' => false]) }}
         </div>
     </section>
 </div>
